aec9856c9608ee78a2152fe86d22adc051b17b05
Milad Faghihi <miladfm@gmail.com>
2025-02-15 23:18:47 +0100

    b

---
+++ _next/static/WqCG-dH34SNvgm9WQm7Hv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-05fdf8b8c020a9d7.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/app/collaboration/page-b101bc22247d5ac1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[304],{4011:function(e,n,s){Promise.resolve().then(s.t.bind(s,3995,23)),Promise.resolve().then(s.t.bind(s,2786,23))}},function(e){e.O(0,[786,995,527,993,744],function(){return e(e.s=4011)}),_N_E=e.O()}]);
